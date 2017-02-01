3040ceccce8f97414c0c6d2c19378962bd90b90a
Michael Croghan <mike@digitalkeg.com>
2017-02-01 05:09:40 -0800

    issue #n/a; added cacheing. Moved salable after event from frontend to global. Added a product save after and quote submit after event, both related to cache clearing

--- app/code/community/Croghan/ShipHero/Model/Observer.php
+++ app/code/community/Croghan/ShipHero/Model/Observer.php
@@ -9,6 +9,7 @@ class Croghan_ShipHero_Model_Observer
      */
     public function isSalableAfterStockCheck ($_observer)
     {
+        //@see Mage_Catalog_Model_Product
         /*
         $object = new Varien_Object(array(
             'product'    => $this,
@@ -39,6 +40,60 @@ class Croghan_ShipHero_Model_Observer
                 $product->setData('is_salable', 0);   
             }
         }
+
+        return $this;
+    }
+
+    /*
+     * cleanCacheAfterCheckout method
+     *
+     * clears cache after a successful sale
+     */
+    public function cleanCacheAfterCheckout ($_observer)
+    {
+        //@see Mage_Sales_Model_Service_Quote
+        //Mage::dispatchEvent('sales_model_service_quote_submit_after', array('order'=>$order, 'quote'=>$quote));
+        $event = $_observer->getEvent();
+        $quote = $event->getQuote();
+
+        // traverse items and clear shiphero cache on all //
+        foreach ($quote->getAllVisibleItems() as $item) {
+            $product = $item->getProduct();
+
+            // shouldn't have to do this, but just in case //
+            if ( ! $product->getStockItem()) {
+                Mage::getModel('cataloginventory/stock_item')->assignProduct($product);
+            }
+
+            // dev log //            
+            Mage::log(sprintf("%s::%s: clearing shiphero cache for product sku %s", __CLASS__, __METHOD__, $product->getSku()), null, "shiphero.log");
+
+            $product->getStockItem()->cleanModelCache(); // clean cache
+        }
+
+        return $this;
+    }
+
+    /*
+     * cleanCacheAfterProductSave method
+     *
+     * clears cache after a product is saved
+     */
+    public function cleanCacheAfterProductSave ($_observer)
+    {
+        //@see Mage_Core_Model_Abstract
+        //MMage::dispatchEvent($this->_eventPrefix.'_load_after', $this->_getEventData());
+        $product = $_observer->getProduct();
+
+        // in most cases will have this //
+        if ( ! $product->getStockItem()) {
+            Mage::getModel('cataloginventory/stock_item')->assignProduct($product);
+        }
+
+        // dev log //
+        Mage::log(sprintf("%s::%s: clearing shiphero cache for product sku %s", __CLASS__, __METHOD__, $product->getSku()), null, "shiphero.log");
+
+        $product->getStockItem()->cleanModelCache(); // clean cache
 
         return $this;
     }
--- app/code/community/Croghan/ShipHero/Model/Stock/Item.php
+++ app/code/community/Croghan/ShipHero/Model/Stock/Item.php
@@ -2,6 +2,8 @@
 
 class Croghan_ShipHero_Model_Stock_Item extends Mage_CatalogInventory_Model_Stock_Item
 {
+    const EXTRA_CACHE_TAG = 'shiphero_cache'; // don't define CACHE_TAG
+
     /**
      * _beforeSave
      */
@@ -75,7 +77,9 @@ class Croghan_ShipHero_Model_Stock_Item extends Mage_CatalogInventory_Model_Stoc
         }
         // only execute if module is enabled and type Id is simple //
         else {
-            if ( ! $this->getData('shiphero_qty')) {
+            $this->_loadQtyCache(); // attempt to load from cache
+
+            if ( ! $this->hasData('shiphero_qty')) {
                 // sku is inconsistent; in either place, sometimes not added to stock_item model //
                 if ( ! $this->getSku() && $this->getProduct()){
                     Mage::log("No SKU, product ({$this->getProduct()->getId()}) exists", null, "shiphero.log");
@@ -94,15 +98,72 @@ class Croghan_ShipHero_Model_Stock_Item extends Mage_CatalogInventory_Model_Stoc
                 //error_log(sprintf("response\n\n%s\n\n", print_r($response,true)));
 
                 // product, shiphero response, warehouse which will come from store map //
-                $availableQty = $this->_helperItem()->getAvailable ($this->getSku(), $response, $this->_helperWarehouse()->getWarehouses());
+                $availableQty = $this->_helperItem()->getAvailable ($this->getSku(), $response, array());
                 //error_log(sprintf("availableQty: %s", $availableQty));
 
                 $this->setData('shiphero_qty', (float)$availableQty);
                 $this->setData('qty', (float)$availableQty);
+
+                $this->_saveQtyCache((float)$availableQty); // save cache //
             }
 
             return $this->getData('qty');
         }
+    }
+
+    /**
+     * _saveQtyCache method
+     *
+     * saves shiphero qty to cache
+     */
+    protected function _saveQtyCache($_qty)
+    {
+        $jsonArr = array('qty' => (float)$_qty);
+
+        Mage::app()->saveCache(json_encode($jsonArr), $this->_getStoreCacheTag());
+    }
+
+    /**
+     * _loadQtyCache method
+     *
+     * tries to load shiphero qty cache
+     */
+    protected function _loadQtyCache()
+    {
+        $jsonStr = Mage::app()->loadCache($this->_getStoreCacheTag());
+
+        if ($jsonStr && ($jsonArr = json_decode($jsonStr,true))) {
+            $qty = $jsonArr['qty'];
+            $this->setData('shiphero_qty', $qty);
+            $this->setData('qty', $qty);
+        }
+    }
+
+    /**
+     * Remove model onject related cache
+     *
+     * @return Mage_Core_Model_Abstract
+     */
+    public function cleanModelCache()
+    {
+        $tags = parent::getCacheTags();
+        $tags[] = $this->_getStoreCacheTag();
+
+        if ($tags !== false) {
+            Mage::app()->cleanCache($tags);
+        }
+
+        return $this;
+    }
+
+    /*
+     * _getStoreCacheTag method
+     *
+     * returns store related cache tag; will double
+     */
+    protected function _getStoreCacheTag()
+    {
+        return sprintf("%s_%s_%s", self::EXTRA_CACHE_TAG, Mage::app()->getStore()->getCode(), $this->getProductId());
     }
 
     /*
@@ -123,15 +184,5 @@ class Croghan_ShipHero_Model_Stock_Item extends Mage_CatalogInventory_Model_Stoc
     protected function _helperItem()
     {
         return Mage::helper('croghan_shiphero/item');
-    }
-
-    /*
-     * _helperWarehouse
-     *
-     * return warehouse helper
-     */
-    protected function _helperWarehouse()
-    {
-        return Mage::helper('croghan_shiphero/warehouse');
     }
 }
